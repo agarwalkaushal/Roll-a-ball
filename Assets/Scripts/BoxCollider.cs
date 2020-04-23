c031988cbed93fe10ee6ae7207f950104b27fe30
Kaushal Agarwal <kaushal.agarwal@oyorooms.com>
2020-04-23 22:45:24 +0530

    Files added

---
+++ Assets/Scripts/BoxCollider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxCollider : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Transform transform = other.gameObject.GetComponent<Transform>();
+
+        Debug.Log(transform.position);
+
+        float prevX = transform.position.x;
+        float prevZ = transform.position.z;
+
+        if(prevZ < 0 && prevX < 0)
+            transform.position = new Vector3(-prevX - 1f, 0.5f, -prevZ - 1f);
+        if (prevZ > 0 && prevX < 0)
+            transform.position = new Vector3(-prevX - 1f, 0.5f, -prevZ + 1f);
+        if (prevZ < 0 && prevX > 0)
+            transform.position = new Vector3(-prevX + 1f, 0.5f, -prevZ - 1f);
+        if (prevZ > 0 && prevX > 0)
+            transform.position = new Vector3(-prevX + 1f, 0.5f, -prevZ + 1f);
+        return;
+    }
+
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Rigidbody rigidB;
+    float speed = 350;
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        rigidB = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 tilt = Input.acceleration;
+        tilt = Quaternion.Euler(90, 0, 0) * tilt;
+        rigidB.AddForce(tilt * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidB.AddForce(Vector3.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidB.AddForce(Vector3.back * speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidB.AddForce(Vector3.right * speed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidB.AddForce(Vector3.left * speed);
+        }
+    }
+}
